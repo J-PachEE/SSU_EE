31f3afb8482eea4980ff0abb90af9fddcb17305c
J-PachEE <jairpacheco8787@gmail.com>
2025-04-17 20:10:32 -0700

    Create Initialization.h

---
+++ Microcontroller_EE310/Assignments/Initialization.h
@@ -0,0 +1,29 @@
+/* 
+ * File:   Initialization.h
+ * Author: jairp
+ *
+ * Created on April 17, 2025, 5:29 PM
+ */
+
+#ifndef INITIALIZATION_H
+#define	INITIALIZATION_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+int Buzz_loop = 0;
+int SECRET_CODE = 0x23;
+int Num1 = 0;
+int Num2 = 0;
+int dig_count = 0;
+int Enter = 0;
+int Full_Num = 0;
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* INITIALIZATION_H */
+
