bb169da91af8a1ba7d5ae335d7fbc752af5d73ad
J-PachEE <jairpacheco8787@gmail.com>
2025-11-03 02:48:05 -0800

    Update IOTDATASLACKmain.cpp

--- EE470/hwAssignments/RGB/IOTDATASLACKmain.cpp
+++ EE470/hwAssignments/RGB/IOTDATASLACKmain.cpp
@@ -1,4 +1,26 @@
+//-------------------------------
+// Title: IFTTT Webhook Trigger via ESP8266
+//-------------------------------
+// Program Details:
+//-------------------------------
+// Purpose: This program monitors a physical button press and sends a PUT request
+// to an IFTTT webhook URL to trigger an online event (e.g., Slack, SMS, Google Sheets).
+// Input: Physical button press (D5)
+// Output: PUT request to IFTTT webhook
+// Date: 11/3/2025
+// Compiler: PlatformIO
+// Author: Jair Pacheco
+// Versions:
+// V1: Initial implementation using HTTPS and manual PUT request
+// V2: Added debounce and serial debug output
+//---------------------------------
+// File Dependencies: ESP8266WiFi.h, WiFiClientSecure.h
+//---------------------------------
 
+
+//---------------------------------
+// Main Program
+//---------------------------------
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
 
@@ -11,12 +33,13 @@ const char* host = "maker.ifttt.com";
 const int httpsPort = 443;
 const char* requestPath = "/trigger/Button_pressed/with/key/lnsgEOIZYpEjYtlc2KFSe9ZxtNinxfgHt1LSm3pWPo2";
 
-// Button pin
+
 const int buttonPin = D5;
 
 void setup() {
+  // Initialize serial monitor and configure button pin
   Serial.begin(9600);
-  pinMode(buttonPin, INPUT); // Pull-down resistor already wired
+  pinMode(buttonPin, INPUT);
   WiFi.begin(ssid, password);
 
   Serial.print("Connecting to WiFi");
@@ -28,6 +51,7 @@ void setup() {
 }
 
 void loop() {
+   // Monitor button press with edge detection
   static bool lastState = LOW;
   bool currentState = digitalRead(buttonPin);
 
@@ -39,6 +63,7 @@ void loop() {
 
     if (client.connect(host, httpsPort)) {
       String url = String(requestPath) + "?value1=Button+Pressed";
+        // Send raw HTTP PUT request
       client.print(String("PUT ") + url + " HTTP/1.1\r\n" +
                    "Host: " + host + "\r\n" +
                    "Connection: close\r\n\r\n");
